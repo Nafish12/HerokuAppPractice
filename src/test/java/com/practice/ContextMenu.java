aefdd3722b92c0b5a91f5b9dbc0c29353497da29
Nafish12 <md.nafisali1@gmail.com>
2025-09-16 10:47:46 -0400

    Context Menu completed

--- src/test/java/com/practice/ContextMenu.java
+++ src/test/java/com/practice/ContextMenu.java
@@ -1,5 +1,36 @@
 package com.practice;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
 public class ContextMenu {
+    public static void main(String[] args) throws InterruptedException{
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+
+        driver.get("https://the-internet.herokuapp.com/context_menu");
+
+        WebElement hotspot = driver.findElement(By.cssSelector("div#hot-spot"));
+        Actions actions = new Actions(driver);
+
+        actions.contextClick(hotspot).perform();
+        wait.until(ExpectedConditions.alertIsPresent());
+
+        String alertText = driver.switchTo().alert().getText();
+        System.out.println(alertText);
+        driver.switchTo().alert().accept();
+
+        driver.close();
+    }
 
 }
